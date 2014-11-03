b5e786a143790fb4e8d667c71d2a0090aa984b83
Andre Cunha <andre.lv.cunha@gmail.com>
2014-11-03 17:08:05 -0200

    Added test file for regression testing.

---
+++ test.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+from __future__ import print_function, unicode_literals, division
+from idd3 import Relation, Engine
+import rules
+import nltk
+
+
+def test():
+    with open('expected.txt', mode='r') as expected_file:
+        expected = expected_file.readlines()
+
+    up_to_index = len(expected)
+
+    graphs = nltk.parse.dependencygraph.DependencyGraph.load('corpus.conll')
+
+    engine = Engine(rules.all_rulesets)
+
+    for i in range(up_to_index):
+        relations = []
+        for relation in graphs[i].nodelist:
+            relations.append(Relation(**relation))
+
+        engine.analyze(relations)
+        props = engine.props
+
+        exp = eval(expected[i])
+
+        assert props == exp
+
+if __name__ == '__main__':
+    test()
